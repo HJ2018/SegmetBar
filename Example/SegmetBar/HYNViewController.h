27c7224922eaec6a129a776aec2411c608fc1796
jie.huang <jie.huang@jiehuangdeMacBook-Pro.local>
2018-04-08 13:25:53 +0800

    Initial commit

---
+++ Example/SegmetBar/HYNViewController.h
@@ -0,0 +1,13 @@
+//
+//  HYNViewController.h
+//  SegmetBar
+//
+//  Created by HJ2018 on 04/08/2018.
+//  Copyright (c) 2018 HJ2018. All rights reserved.
+//
+
+@import UIKit;
+
+@interface HYNViewController : UIViewController
+
+@end
---
+++ Example/SegmetBar/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SegmetBar
+//
+//  Created by HJ2018 on 04/08/2018.
+//  Copyright (c) 2018 HJ2018. All rights reserved.
+//
+
+@import UIKit;
+#import "HYNAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HYNAppDelegate class]));
+    }
+}
